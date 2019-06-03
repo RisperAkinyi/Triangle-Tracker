d1331e6b161cb6a70c243a738a9e017413a9db67
Risper Akinyi <email@example.com>
2019-06-03 11:31:41 +0300

    add the function of a scalene triangle

--- main.js
+++ main.js
@@ -21,7 +21,10 @@ else if (side1 === side2 && side2 === side3 && side3 === side1 ){
 else if (side1 === side2 || side2 === side3 || side3 === side1 ){
     alert("hooray! thats an isosceles triangle");
 }
-    
-
+////Function for evaluating a scalene triangle
+else
+{
+   alert("you are a genius, its a scalene triangle");
+}  
 
 }
